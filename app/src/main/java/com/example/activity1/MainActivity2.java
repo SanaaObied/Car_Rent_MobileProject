fe002d688a2598fc3d055bc0e8f4708e368cc523
SanaaObied <sanaobied2@gmail.com>
2024-05-31 09:33:18 -0700

    this is first virtion

---
+++ app/src/main/java/com/example/activity1/MainActivity2.java
@@ -0,0 +1,40 @@
+package com.example.activity1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import  android.content.Intent;
+import android.view.View;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Button adminButton = findViewById(R.id.btnADMIN);
+        Button customerButton = findViewById(R.id.btnCustomer);
+
+        adminButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Perform action for admin button click
+                // For example, start AdminActivity
+             //  Intent intent = new Intent(MainActivity2.this, AdminActivity.class);
+                //startActivity(intent);
+            }
+        });
+
+        customerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Perform action for customer button click
+                // For example, start CustomerActivity
+             //   Intent intent = new Intent(MainActivity2.this, CustomerActivity.class);
+              //  startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/activity1/MainActivity7.java
@@ -0,0 +1,77 @@
+package com.example.activity1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.List;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.List;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MainActivity7 extends AppCompatActivity {
+    private Spinner spnMenu;
+    private RecyclerView recyclerView;
+    private CustomListAdapter adapter;
+    private List<Menultem> itemsList;
+    private DatabaseItems databaseItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main7);
+
+        spnMenu = findViewById(R.id.spinner);
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        databaseItems = new DatabaseItems();
+        itemsList = databaseItems.getAllMenuItems();
+
+        adapter = new CustomListAdapter(this, itemsList);
+        recyclerView.setAdapter(adapter);
+
+        setupSpinner();
+    }
+
+    private void setupSpinner() {
+        String[] categories = databaseItems.getCategories();
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, categories);
+        spnMenu.setAdapter(spinnerAdapter);
+
+        spnMenu.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedCategory = categories[position];
+                itemsList.clear();
+                if (selectedCategory.equals("All")) {
+                    itemsList.addAll(databaseItems.getAllMenuItems());
+                } else {
+                    itemsList.addAll(databaseItems.getMenuItems(selectedCategory));
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Do nothing
+            }
+        });
+    }
+}
