8f3b841dfbf42d7a34cdc21281c9085a74fd4224
SanaaObied <sanaobied2@gmail.com>
2024-06-06 13:03:52 -0700

    this is second virtion

---
+++ app/src/main/java/com/example/activity1/Car.java
@@ -0,0 +1,19 @@
+package com.example.activity1;
+
+public class Car {
+    private String model;
+    private String image;
+
+    public Car(String model, String image) {
+        this.model = model;
+        this.image = image;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getImage() {
+        return image;
+    }
+}
